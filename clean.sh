51bfcfe49c51b42a22c0d866e65a2149ab17762a
cannnAvar <72388136+cannnAvar@users.noreply.github.com>
2023-05-16 11:14:46 +0000

    cleaning script

---
+++ clean.sh
@@ -0,0 +1,6 @@
+rm -r ./CMakeFiles
+rm ./CMakeCache.txt
+rm ./Makefile
+rm ./cmake_install.cmake
+rm ./compile_commands.json
+rm ./rog
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
-#include <iostream>
 #include "kutuphane.h"
 
 int main()
 {
-    std::cout << "Merhaba Dünya\n";
-    std::cout << topla(1,1) << std::endl;
     return 0;
 }
